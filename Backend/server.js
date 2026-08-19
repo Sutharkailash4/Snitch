d01c7e04f949693efd7d74e65c9dea379a51e252
kailash suthar <kailashsuthar4488@gmail.com>
2026-08-19 08:54:13 +0530

    SetupBasic srver

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+function removeDuplicates(arr) {
+    let previous = arr[0];
+    for(let i=1; i<arr.length; i++) {
+
+    }
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+console.log(a);
+console.log(b);
+console.log(c);
