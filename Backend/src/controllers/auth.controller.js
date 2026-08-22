4f24181020560df8965c30a75e123dd0cd7883c9
Kailash Suthar <kailash.ksuthar@mavinglobal.com>
2026-08-22 20:38:58 +0530

    Express Validator

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,9 +1,64 @@
 import userModel from "../models/auth.model.js";
-import JWT from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 const authRegisterController = async (req, res) => {
     try {
+        const {fullName, email, password, mobileNumber, role} = req.body;
+
+        const isUserAlreadyExists = await userModel.find({
+            email : email            
+        });
+
+        if(isUserAlreadyExists) {
+            return res.status(404).json({
+                message : "User already exists"
+            })
+        }
+
+        const user = await userModel.create({
+            fullName : fullName,
+            email : email,
+            password :  password,
+            mobileNumber : mobileNumber,
+            role : role
+        });
+
+        const accessToken = jwt.sign({
+            Fullname : user.fullName,
+            id : user._id,
+            email : user.email
+        },
+        process.env.JWT_ACCESS_TOKEN,
+        {
+            expiresIn : "3h"
+        }
+    )
+
+    const refreshToken = jwt.sign({
+        Fullname : user.fullName,
+            id : user._id,
+            email : user.email
+    },
+    process.env.JWT_REFRESH_TOKEN,
+    {
+        expiresIn : "7d"
+    }
+)
+
+    res.cookie("access_token", accessToken, {
+        httpOnly : true,
+        secure : true,
+        sameSite : "strict"
+    });
+
+        res.cookie("refresh_token", refreshToken, {
+        httpOnly : true,
+        secure : true,
+        sameSite : "strict"
+    });
+
+    
 
     } catch (error) {
         res.status(400).json({
