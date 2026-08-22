9f955fd3085189e0b6bfb979e836434cf3dab6c8
Kailash Suthar <kailash.ksuthar@mavinglobal.com>
2026-08-22 20:51:40 +0530

    Fxi express validatr

--- Backend/src/validators/auth.validator.js
+++ Backend/src/validators/auth.validator.js
@@ -15,57 +15,52 @@ const validateRequests = (req, res, next) => {
 
 const validateRegister = [
     body("fullName")
-    .trim()
-    .isEmpty()
-    .withMessage("Full Name is required")
-    .isLength({min : 2 , max : 50})
-    .withMessage("Full Name must be between 2 to 50 characters"),
+        .trim()
+        .notEmpty()
+        .withMessage("Full Name is required")
+        .isLength({ min: 2, max: 50 })
+        .withMessage("Full Name must be between 2 to 50 characters"),
 
     body("email")
-    .trim()
-    .isEmpty()
-    .withMessage("Email is required")
-    .isEmail()
-    .withMessage("Please provide a valid email")
-    .normalizeEmail(),
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Please provide a valid email")
+        .normalizeEmail(),
 
     body("password")
-    .trim()
-    .isEmpty()
-    .withMessage("Password is required")
-    .isLength({min: 8, max: 128})
-    .withMessage("Password must be beetwen 8 to 128 characters"),
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8, max: 128 })
+        .withMessage("Password must be between 8 to 128 characters"),
 
-    body("moblieNumber")
-    .trim()
-    .isEmpty()
-    .withMessage("Mobie number is required")
-    .isMobilePhone("en-IN")
-    .withMessage("Enter a valid Indian mobile number"),
+    body("mobileNumber")
+        .trim()
+        .notEmpty()
+        .withMessage("Mobile number is required")
+        .isMobilePhone("en-IN")
+        .withMessage("Enter a valid Indian mobile number"),
 
     validateRequests
 ];
 
 const validateLogin = [
     body("email")
-    .trim()
-    .isEmpty()
-    .withMessage("Email is required")
-    .isEmail()
-    .withMessage("Please provide a valid email")
-    .normalizeEmail(),
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Please provide a valid email")
+        .normalizeEmail(),
 
     body("password")
-    .trim()
-    .isEmpty()
-    .withMessage("Password is required")
-    .isLength({min: 8, max: 128})
-    .withMessage("Password must be beetwen 8 to 128 characters"),
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8, max: 128 })
+        .withMessage("Password must be between 8 to 128 characters"),
 
-    validateRequests  
+    validateRequests
 ];
-
-export {
-    validateRegister,
-    validateLogin
-}                                                                                                           
