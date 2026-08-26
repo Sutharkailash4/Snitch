ae358d207c7703f2b255b12f215d630adb64a386
Kailash Suthar] <kailashsuthar4488@gmail.com>
2026-08-26 22:31:23 +0530

    Changes

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -2,8 +2,12 @@ import express from "express";
 
 const authRoutes = express();
 
+// Controllers and Validator
+
 import { authGetMeController, authLoginController, authRegisterController } from "../controllers/auth.controller.js";
 import { validateRegister, validateLogin } from "../validators/auth.validator.js";
+
+// Authentication Routes
 
 authRoutes.post("/register", validateRegister, authRegisterController);
 authRoutes.post("/login", validateLogin, authLoginController);
